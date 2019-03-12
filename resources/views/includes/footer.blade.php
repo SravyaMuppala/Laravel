b14d17ffab4be0320599da8f7a3f2eee7342537d
sravya <saisravyam@gmail.com>
2019-03-12 18:05:19 -0400

    Laravel Setup

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,4 @@
 <footer class = "container">
-<div class="row mt-4 mb-4"  class="font-weight-light">
-    &copy; 2017-2018 Learana, Inc. &middot; <a href="#"  class="font-weight-light">Privacy</a> &middot; <a href="#"  class="font-weight-light">Terms</a>
-</div>
+    <p class="float-right"><a href ="#">Back to top</a></p>
+    <p>&copy; 2017-2018 Learana,Inc. &middot; <a href ="#">Privacy</a> &middot; <a href="#">Terms</a></p>
 </footer>
