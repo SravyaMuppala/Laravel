19aa45a0f6b311a42e10999d52086fa8f09c5ef0
sravya <saisravyam@gmail.com>
2019-03-12 21:07:34 -0400

    Laravel Setup

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.default')
 @section('content')
+<br>
+<br>
+<br>
 
 <h1>Hello World Welcome to my About Page</h1>
 
-@stop;
+@stop
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.default')
 @section('content')
+<br>
+<br>
+<br>
 
+<h1>Hello World Welcome to my Home Page</h1>
 
-    <h1>Hello World Welcome to my Home Page</h1>
-
-@stop;
+@stop
